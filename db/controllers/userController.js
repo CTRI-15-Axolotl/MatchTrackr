71ec1d234da0e7601ea8375da3bf6cbfb6c80383
ychiu5896 <ychiu5896@gmail.com>
2023-04-08 19:25:49 -0500

    finished newUsers

--- db/controllers/userController.js
+++ db/controllers/userController.js
@@ -4,9 +4,33 @@ const UserController = {};
 
 UserController.getUsers = (req, res, next) => {
   db.query('SELECT * FROM users').then((result) => {
-    console.log(result.rows[0]);
+    res.locals.getUsers = result.rows;
+    console.log(result.rows);
     next();
   });
 };
+//////////////////////////////////////////////////////////////
+//only need username as primary key, remove id since unnecessary
+//created a new table under favlist that uses favorite as a foreign key
+//for new users
+
+UserController.newUser = (req,res,next) => {
+  const queryStr = "INSERT INTO users (username,password) VALUES ($1,$2) RETURNING*";
+  const {username, password} = req.body
+  db.query(queryStr,[username,password])
+  .then((result) => {
+    res.locals.newUser = result.rows;
+    next();
+  })
+  .catch((err) => {
+    next({
+      log: `UserController.newUser: ERROR: ${err} `,
+      message: {err : `Error occured in UserController.newUser`}
+    })
+  });
+}
+
 
 module.exports = UserController;
+
+////////////////////////////////////////////////////////////////
--- db/route/routes.js
+++ db/route/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const UserController = require('../controllers/userController.js');
 const router = express.Router();
 
-router.get('/', UserController.getUsers, (req, res) => res.status(200));
+router.get('/', UserController.getUsers, (req, res) => res.status(200).json(res.locals.getUsers));
+
+router.post('/',UserController.newUser, (req,res) => res.status(200).json(res.locals.newUser));
 
 module.exports = router;
